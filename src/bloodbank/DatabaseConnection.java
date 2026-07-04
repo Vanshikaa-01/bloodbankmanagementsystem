46a9db5631ca36429c7a172d53b7a56d5b88529f
Vanshikaa-01 <agarwalvanshika1504@gmail.com>
2026-07-05 00:11:34 +0530

    Update DatabaseConnection.java

--- src/bloodbank/DatabaseConnection.java
+++ src/bloodbank/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/blood_bank";
     private static final String USER = "root";
-    private static final String PASSWORD = "Va@150904"; // <-- Change this
+    private static final String PASSWORD = ""; // <-- Change this
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
